87e4d07d5643de96957393d78c7a638c5921514a
Jordan Thayer <jtthayer@sep.com>
2024-02-08 08:38:55 -0500

    Stub of defining a whole suit

---
+++ suit_slug.py
@@ -0,0 +1,69 @@
+from card import FaceValue
+
+card_slugs = {
+    FaceValue.ACE: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.TWO: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.THREE: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.FOUR: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.FIVE: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.SIX: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.SEVEN: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.EIGHT: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.NINE: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.TEN: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.JACK: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.QUEEN: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    },
+    FaceValue.KING: {
+        "boiler_plate": """""",
+        "fixed_prompts": [],
+        "random_prompts": []
+    }
+}
