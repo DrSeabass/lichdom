b4cd4e18e6524218644095a704ce99eb7ddbaaf7
Jordan Thayer <jordan.thayer@gmail.com>
2024-02-11 16:28:35 -0500

    Fix Scheme / Scry cost display

--- game/cards/scheme_scry.py
+++ game/cards/scheme_scry.py
@@ -2,15 +2,33 @@ from enum import Enum
 import random
 from game.cards.card import Card, CardType, FaceValue, Suit
 
+
 class PaymentType(Enum):
     RESOLVE = 0
     DOOM = 1
     CARD = 2
 
+    def __str__(self):
+        match self:
+            case PaymentType.RESOLVE:
+                return "Lose 1 Resolve"
+            case PaymentType.DOOM:
+                return "Gain 1 Doom"
+            case PaymentType.CARD:
+                return "Sacrifice a Card"
+
+
 class Payment:
     def __init__(self, type, card=None):
-        self.type : PaymentType = type
+        self.type: PaymentType = type
         self.card = card
+
+    def __str__(self):
+        output = "{}".format(self.type)
+        if self.card:
+            output = "{}: {}".format(output, self.card)
+        return output
+
 
 class SchemeScry(Card):
     def __init__(self, suit: Suit, value: FaceValue, slug={}):
@@ -56,7 +74,7 @@ class SchemeScry(Card):
         deck.push_many(certain_future)
 
     def find_possible_payment(self, player):
-        options = [ Payment(PaymentType.RESOLVE), Payment(PaymentType.DOOM) ]
+        options = [Payment(PaymentType.RESOLVE), Payment(PaymentType.DOOM)]
         for card in player.hand:
             if card.cardType == CardType.INFLUENCE or card.cardType == CardType.SCHEME_SCRY:
                 if card is not self:
