45a4b51bc21ed1cd7aa4951376a5f702d171031c
Jordan Thayer <jordan.thayer@gmail.com>
2024-02-11 20:28:01 -0500

    Change how game end is tracked to know how game ended

--- game/game.py
+++ game/game.py
@@ -12,6 +12,14 @@ class UserPromptBase(Enum):
     ATTEMPT_LICHDOM = 2
     SCHEME_SCRY = 3
 
+# TODO: Hang game end string off of a terminal node for the state machine, consolidating where output happens
+class TerminalCondition(Enum):
+    CATACLYSM = 0
+    LOST_RESOLVE = 1
+    FAILED_LICHDOM = 2
+    LICHDOM = 3
+    GODHOOD = 4
+    NOT_TERMINAL = 5
 
 class UserPrompt:
 
@@ -56,7 +64,7 @@ class Game:
         self.player: Player = Player()
         self.deck: Deck = Deck()
         self.previous_card: Card = None
-        self.continue_game: bool = True
+        self.terminal = TerminalCondition.NOT_TERMINAL
         self.deck.shuffle()
 
     def get_step_actions(self):
@@ -135,26 +143,31 @@ class Game:
             print(
                 """You die horribly, your flesh boils and your bones crumble to dust,
 cursing the land where you attempted the foul ritual.""")
+            self.terminal = TerminalCondition.FAILED_LICHDOM
         elif 4 < score <= 8:
             print(
                 """You become a wraith, a half ethereal horror beyond the comprehension of other mortals.
 You will inevitably lose your mind and sanity in the many years ahead haunting your lair, becoming only a monster with
 no other ambitions besides consuming human souls. A beast far removed from your goal of power.""")
+            self.terminal = TerminalCondition.FAILED_LICHDOM
         elif 8 < score <= 11:
             print(
                 """Your foul ritual partially succeeds, giving you immense power and an extremely long life.
 You will live 12 13 hundreds of years, command armies and establish cults that will outlive you…
 but you are not immortal. Your body will eventually wither and die, everything you built will crumble to dust,
 and your name will be forgotten. You are only mortal after all.""")
+            self.terminal = TerminalCondition.FAILED_LICHDOM
         elif 11 < score < 19:
             print(
                 """You made it. Your immortal soul will linger in this world forever, just enough time to discover
 all the secrets of the cosmos. Your body may decay with time, but you will find younger vessels as the ages pass by.
 You are a lich.""")
+            self.terminal = TerminalCondition.LICHDOM
         else:
             print(
                 """Time doesn’t have meaning any more. Ages come and go, empires rise and fall, and you stand above
 them all while learning the most corrupting secrets of the void beyond reality. You have become a god""")
+            self.terminal = TerminalCondition.GODHOOD
 
     def process_card(self, card):
         if not (
@@ -168,7 +181,8 @@ them all while learning the most corrupting secrets of the void beyond reality. 
                 card.cardType == CardType.PLOTS_CURSES
         ):
             card.take_actions(self.player, self.deck)
-        self.continue_game = self.player.resolve > 0
+        if self.player.resolve <= 0:
+            self.terminal = TerminalCondition.LOST_RESOLVE
 
     def step(self):
         possible_actions = self.get_step_actions()
@@ -180,17 +194,25 @@ them all while learning the most corrupting secrets of the void beyond reality. 
                 if drawn_card.base == DrawStepResultBase.CARD:
                     self.process_card(drawn_card.card)
                 else:  # Magical Cataclysm
-                    # TODO Handle end of world via cataclysm
-                    print("PLACE HOLDER END OF WORLD TEXT")
-                    self.continue_game = False
+                    self.terminal = TerminalCondition.CATACLYSM
             case UserPromptBase.DISPLAY_PLAYER_HAND:
                 print(self.player.player_hand_str())
             case UserPromptBase.SCHEME_SCRY:
                 selected_action.card.take_actions(self.player, self.deck)
             case UserPromptBase.ATTEMPT_LICHDOM:
                 self.attempt_lichdom()
-                self.continue_game = False
 
     def play(self):
-        while self.continue_game:
+        while self.terminal == TerminalCondition.NOT_TERMINAL:
             self.step()
+        match self.terminal:
+            case TerminalCondition.CATACLYSM:
+                print("Placeholder cataclysm string")
+            case TerminalCondition.LOST_RESOLVE:
+                print("Placeholder lost resolve string")
+            case TerminalCondition.FAILED_LICHDOM:
+                print("Placeholder lost resolve string")
+            case TerminalCondition.LICHDOM:
+                print("Placeholder lichdom string")
+            case TerminalCondition.GODHOOD:
+                print("Placeholder godhood string")
