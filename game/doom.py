b92b4c7ad18e0a8982afd10bf64f6f097c0e933a
Jordan Thayer <jordan.thayer@gmail.com>
2024-02-18 09:04:35 -0500

    Define doom marks from game text

---
+++ game/doom.py
@@ -0,0 +1,18 @@
+
+body_doom = [
+    "You develop reptile eyes that can only see in darkness.",
+    "Strong light burns your skin, which turns to ash.",
+    "Your flesh rots and worms burrow under your skin.",
+    "Black horns grow painfully from your skull whenever you feel hate.",
+    "You must feed on human blood to avoid withering to a lifeless husk.",
+    "Your body is weak and can no longer support you. You must rely on alchemy, sorcery, or servants to move."
+]
+
+mind_doom = [
+    "You become paranoid, believing enemies surround you and friends conspire against you.",
+    "Your insomnia makes restful sleep an insurmountable challenge without drugs distilled from poisonous flowers.",
+    "You are full of hate for your fellow mortals. The realisation that you are just like them makes you sick and bitter.",
+    "You become forgetful, increasingly relying on your notes and journal to continue your quest. Your passions remain, but the memories fade.",
+    "You start to lose hope that you will ever become immortal. Doubts plague your thoughts.",
+    "Your arrogance reaches new limits. You see other mortals as soulless automatons to your disposal."
+]
